68141c25fa2a7a7c9159415a2015ee9ba9dece83
jsmit1993 <156153441+jsmit1993@users.noreply.github.com>
2026-08-28 08:19:36 -0400

    add style to initial load
    forest restaurant should have green background but needs white lettering
    for visibility.

--- src/initialLoad.js
+++ src/initialLoad.js
@@ -14,8 +14,22 @@ function describeVibe() {
 function storePhone() {
     const pNumber = document.createElement('div');
     pNumber.classList.add('pnumber');
-    pNumber.textContent = '808-555-5050';
+    pNumber.textContent = 'Phone: 808-555-5050';
     return pNumber;
+}
+
+function storeHours() {
+    const hours = document.createElement('div');
+    hours.classList.add('hours');
+    hours.textContent = 'Daily 7 AM - 9 PM';
+    return hours;
+}
+
+function address() {
+    const location = document.createElement('div');
+    location.classList.add('location');
+    location.textContent = '100 Forest Road'
+    return location;
 }
 
 function loadHome(container) {
@@ -24,6 +38,8 @@ function loadHome(container) {
     container.appendChild(headerLine());
     container.appendChild(describeVibe());
     container.appendChild(storePhone());
+    container.appendChild(storeHours());
+    container.appendChild(address());
 }
 
 export { loadHome }
